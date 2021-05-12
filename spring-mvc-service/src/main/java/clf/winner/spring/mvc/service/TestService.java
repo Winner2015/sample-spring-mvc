b860b11a380e6419243609fbb4218055d07a90af
chenlongfei <496221132@qq.com>
2021-05-12 23:41:32 +0800

    init

---
+++ spring-mvc-service/src/main/java/clf/winner/spring/mvc/service/TestService.java
@@ -0,0 +1,11 @@
+package clf.winner.spring.mvc.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+    public String getName () {
+        return "Winner";
+    }
+}
